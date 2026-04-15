8edea1e298d20621f8904d210a87c9b72a12b034
Archana G <128804874+Ach16@users.noreply.github.com>
2026-04-15 16:34:28 +0530

    Add files via upload

---
+++ 15-04-26_python/passingParams.py
@@ -0,0 +1,17 @@
+def add(a,b):
+    result=a+b
+    print("Sum: ",result)
+def sub(a,b):
+    result=a-b
+    print("Difference: ",result)
+def mul(a,b):
+    result=a*b
+    print("Multiplication: ",result)
+def div(a,b):
+    result=a/b
+    print("Division: ",result)
+
+add(10,20)
+sub(20,10)
+mul(10,20)
+div(20,10)
