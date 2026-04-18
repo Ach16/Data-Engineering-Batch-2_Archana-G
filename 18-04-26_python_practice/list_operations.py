3850961fcd8f1ca47323e094117a9f532ed4c01c
Archana G <128804874+Ach16@users.noreply.github.com>
2026-04-18 11:52:27 +0530

    Update list_operations.py

--- 18-04-26_python_practice/list_operations.py
+++ 18-04-26_python_practice/list_operations.py
@@ -1,1 +1,57 @@
+#list
+numbers=[10,20,30,40,50]
+print(numbers)
 
+fruits=["apple","banana","mango","orange"]
+print(fruits[0])
+print(fruits[2])
+
+#access from the end
+print(fruits[-1])
+print(fruits[-2])
+
+#modify list
+numbers[1]=200
+print(numbers)
+
+#add an element
+numbers.append(40)
+print(numbers)
+
+#insert
+numbers.insert(2,300)
+print(numbers)
+
+#remove
+numbers.remove(30)
+print(numbers)
+
+#remove last element
+numbers.pop()
+print(numbers)
+
+#print by iterating
+for num in numbers:
+    print(num)
+
+#check the existence of a value in a list
+if "banana" in fruits:
+    print("Banana exists")
+if 200 in numbers:
+    print("Exists")
+
+#slicing
+print(numbers[1:3])
+
+#reverse a list
+numbers.reverse()
+print(numbers)
+
+#sort a list
+numbers.sort()
+print(numbers)
+
+#aggregation
+print(sum(numbers))
+print(max(numbers))
+print(min(numbers))
